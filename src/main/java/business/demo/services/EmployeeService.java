151feac54009a1ccc7bdf33ce10735fc34d50f91
measmeas1 <mabmeas12@gmail.com>
2025-02-19 18:21:50 +0700

    create employee service

--- src/main/java/business/demo/services/EmployeeService.java
+++ src/main/java/business/demo/services/EmployeeService.java
@@ -1,5 +1,37 @@
 package business.demo.services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import business.demo.entities.Employee;
+import business.demo.repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class EmployeeService {
-  
+  private final EmployeeRepository employeeRepository;
+
+  public Employee addEmployee(Employee employee){
+    return employeeRepository.save(employee);
+  }
+
+  public List<Employee> getAllEmployees(){
+    return employeeRepository.findAll();
+  }
+
+  public Employee updateEmployee(Employee newEmployee, Long id){
+    return employeeRepository.findById(id).map(emp -> {
+      emp.setName(newEmployee.getName());
+      emp.setPosition(newEmployee.getPosition());
+      emp.setSalary(newEmployee.getSalary());
+      return employeeRepository.save(emp);
+    }).orElseThrow(() -> new RuntimeException("Employee not found"));
+  }
+
+  public void deleteEmployee(Long id) {
+    employeeRepository.deleteById(id);
+  }
+
 }
